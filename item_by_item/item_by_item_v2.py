9be90b614a2c9e6b6e590cbe37fb1bfc0e6d749b
Bartek Kroczek <bartek.kroczek@gmail.com>
2018-03-21 15:30:31 +0100

    Item-vise ready for check !

--- item_by_item/item_by_item_v2.py
+++ item_by_item/item_by_item_v2.py
@@ -12,10 +12,12 @@ from os.path import join
 import yaml
 import numpy as np
 import collections
+import time
 from tqdm import tqdm
 
 os.chdir(
-    join('..', '..', '..', 'Dropbox', 'Data', 'FAN_ET', 'Badanie P', '2017-05-06_Badanie_P', 'BadanieP_FAN_ET', 'Scripts'))
+    join('..', '..', '..', 'Dropbox', 'Data', 'FAN_ET', 'Badanie P', '2017-05-06_Badanie_P', 'BadanieP_FAN_ET',
+         'Scripts'))
 # %%
 ROIS = {
     'P1': [(-  15, 280), (235, 30)],
@@ -28,6 +30,14 @@ ROIS = {
     'E': [(+ 955, 870), (1205, 630)],
     'F': [(+1445, 870), (1695, 630)]
 }
+
+ROIS_ORDER = ['P1', 'P2', 'P3', 'A', 'B', 'C', 'D', 'E', 'F']
+
+
+def where_in_list(where, what):
+    bound = len(where)
+    tmp = map(lambda x: x['name'] == what, where)
+    return [idx for idx, _ in zip(range(bound), tmp) if _]
 
 
 def in_roi(df, roi):
@@ -64,9 +74,11 @@ raw_files = os.listdir(RAW_FOLDER)
 yaml_files = os.listdir(YAML_FOLDER)
 
 ID_GF_WMC = pd.read_csv(join('..', 'results', 'ID_GF_WMC.csv'))
+import random
 
+# sacc_files = [random.choice(sacc_files)]
 with tqdm(total=len(sacc_files)) as pbar:
-    for part_id in sacc_files: # for each participant
+    for part_id in sacc_files:  # for each participant
         pbar.set_postfix(file=part_id)
         pbar.update(1)
 
@@ -76,6 +88,7 @@ with tqdm(total=len(sacc_files)) as pbar:
         sacc_idx = sacc_data.block.unique()
 
         beh_data = pd.read_csv(os.path.join(BEH_FOLDER, part_id + '_beh.csv'))
+        beh_data['corr_and_accept'] = (beh_data['corr'] & beh_data['ans_accept'] & (beh_data['rt'] > 10.0))
 
         fix_data = pd.read_csv(os.path.join(FIX_FOLDER, part_id + '_fix.csv')).drop('Unnamed: 0', 1)
         fix_idx = fix_data.block.unique()
@@ -107,7 +120,7 @@ with tqdm(total=len(sacc_files)) as pbar:
         index = [x for x in index if x <= 45]
         # %%
 
-        avg_corr = beh_data[beh_data.exp == 'experiment']['corr'].mean()
+        avg_corr = beh_data[(beh_data.exp == 'experiment')]['corr_and_accept'].mean()
 
         trs = list()  # total
         ers = list()  # error
@@ -137,16 +150,13 @@ with tqdm(total=len(sacc_files)) as pbar:
         gf = gf_wmc['GF'].values[0]
         wmc = gf_wmc['WMC'].values[0]
 
-        for idx in index:
+        for idx in index:  # for each item
             part_result = collections.OrderedDict()
             sacc_item = sacc_data[sacc_data.block == idx]
             fix_item = fix_data[fix_data.block == idx]
             raw_item = raw_data[raw_data.block == idx]
             beh_item = beh_data.ix[idx - 1]
             problem = problems[idx - 1]
-
-            if beh_item.rt == -1:  # no ans selected
-                continue
 
             # common for all items
             part_result['ID'] = part_id
@@ -163,18 +173,84 @@ with tqdm(total=len(sacc_files)) as pbar:
             part_result['RESP'] = 'COR' if beh_item.choosed_option == 'D1' else 'ERR'
             part_result['LAT'] = int(round(beh_item.rt))
 
-            #rs
-            choosed_option = beh_data.ix[idx - 1]['choosed_option']
-            problem = problems[idx - 1]['matrix_info']
-            err = not beh_data.ix[idx - 1]['corr']
+            # rs
+            choosed_option = beh_item['choosed_option']
+            prob = problem['matrix_info']
+
             if choosed_option == '-1':
-                continue
-
-            denom = np.sum([len(x['elements_changed']) for x in problem[1]['parameters']])
-            counter = [x for x in problem if x['name'] == choosed_option][0]['parameters']
-            counter = np.sum([len(x['elements_changed']) for x in counter])
-
-            if choosed_option == 'D2':  # some magic
-                rs = ((counter - 1) / denom) + 0.02
+                rs = -1
             else:
-                rs = counter / denom
+                denom = np.sum([len(x['elements_changed']) for x in prob[1]['parameters']])
+                counter = [x for x in prob if x['name'] == choosed_option][0]['parameters']
+                counter = np.sum([len(x['elements_changed']) for x in counter])
+
+                if choosed_option == 'D2':  # some magic
+                    rs = ((counter - 1) / denom) + 0.02
+                else:
+                    rs = counter / denom
+            part_result['RS'] = rs
+            part_result['PUP_SIZE'] = raw_item.ps.mean()
+
+            # NT_PR (podzielone przez 3, tzn. wartość per opcja)
+            # NT_COR
+            # NT_SE (podzielone przez 2, tzn. wartość per opcja)
+            # NT_BE (podzielone przez 2, tzn. wartość per opcja)
+            # NT_CON
+            level = LEV_TO_LAB[beh_item.answers]
+
+            # high-lebel rois definitions
+
+            cor_roi = ROIS_ORDER[where_in_list(problem['matrix_info'], 'D1')[0]]
+
+            se_roi = {'EASY': 'D4', 'MEDIUM': 'D3', 'HARD': 'D2'}[level]
+            be_roi = {'EASY': 'D5', 'MEDIUM': 'D4', 'HARD': 'D3'}[level]
+
+            se_roi = [ROIS_ORDER[x] for x in where_in_list(problem['matrix_info'], se_roi)]
+            be_roi = [ROIS_ORDER[x] for x in where_in_list(problem['matrix_info'], be_roi)]
+
+            con_roi = ROIS_ORDER[where_in_list(problem['matrix_info'], 'D6')[0]]
+
+            # saccades
+            sacc_ends_in_pr = pd.concat([in_roi(sacc_item[['exp', 'eyp']], ROIS[roi]) for roi in ['P1', 'P2', 'P3']],
+                                        axis=1).any(axis=1)
+            sacc_ends_in_corr = in_roi(sacc_item[['exp', 'eyp']], ROIS[cor_roi])
+            sacc_ends_in_se = pd.concat([in_roi(sacc_item[['exp', 'eyp']], ROIS[roi]) for roi in se_roi], axis=1).any(
+                axis=1)
+            sacc_ends_in_be = pd.concat([in_roi(sacc_item[['exp', 'eyp']], ROIS[roi]) for roi in be_roi], axis=1).any(
+                axis=1)
+            sacc_ends_in_con = in_roi(sacc_item[['exp', 'eyp']], ROIS[con_roi])
+
+            part_result['NT_PR'] = sacc_ends_in_pr.sum() / 3.0
+            part_result['NT_COR'] = sacc_ends_in_corr.sum()
+            part_result['NT_SE'] = sacc_ends_in_se.sum() / 2.0
+            part_result['NT_BE'] = sacc_ends_in_be.sum() / 2.0
+            part_result['NT_CON'] = sacc_ends_in_con.sum()
+
+            # fixations
+            fix_in_pr = pd.concat([in_roi(fix_item[['axp', 'ayp']], ROIS[x]) for x in ['P1', 'P2', 'P3']], axis=1).any(
+                axis=1)
+            fix_in_corr = in_roi(fix_item[['axp', 'ayp']], ROIS[cor_roi])
+            fix_in_se = pd.concat([in_roi(fix_item[['axp', 'ayp']], ROIS[roi]) for roi in se_roi], axis=1).any(axis=1)
+            fix_in_be = pd.concat([in_roi(fix_item[['axp', 'ayp']], ROIS[roi]) for roi in be_roi], axis=1).any(axis=1)
+            fix_in_con = in_roi(fix_item[['axp', 'ayp']], ROIS[con_roi])
+
+            part_result['FIX_PR'] = fix_item[fix_in_pr].dur.sum() / 1000.0
+            part_result['FIX_COR'] = fix_item[fix_in_corr].dur.sum() / 1000.0
+            part_result['FIX_SE'] = fix_item[fix_in_se].dur.sum() / 1000.0
+            part_result['FIX_BE'] = fix_item[fix_in_be].dur.sum() / 1000.0
+            part_result['FIX_CON'] = fix_item[fix_in_con].dur.sum() / 1000.0
+
+            part_result['DUR_PR'] = fix_item[fix_in_pr].dur.mean() / 1000.0
+            part_result['DUR_COR'] = fix_item[fix_in_corr].dur.mean() / 1000.0
+            part_result['DUR_SE'] = fix_item[fix_in_se].dur.mean() / 1000.0
+            part_result['DUR_BE'] = fix_item[fix_in_be].dur.mean() / 1000.0
+            part_result['DUR_CON'] = fix_item[fix_in_con].dur.mean() / 1000.0
+
+            RESULTS.append(part_result)
+
+res = pd.DataFrame(RESULTS)
+res = res.fillna(0)
+dat = time.localtime()
+filename = '{}_{}_{}_{}:{}'.format(dat.tm_year, dat.tm_mon, dat.tm_mday, dat.tm_hour, dat.tm_min)
+res.to_csv(join('results', 'item_wise_' + filename + '.csv'))
+res.to_excel(join('results', 'item_wise_' + filename + '.xlsx'))
