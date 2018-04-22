7b7ed6c5c6a37a60206bcd1784c0ea7887d8a1eb
Bartek Kroczek <bartek.kroczek@gmail.com>
2018-04-22 22:16:21 +0200

    Bug fixed.

--- dynamics/dynamics.py
+++ dynamics/dynamics.py
@@ -2,20 +2,23 @@ import pandas as pd
 
 import os
 from os.path import join
+from enum import Enum, auto
 import yaml
 import numpy as np
 import time
+import random
 from tqdm import tqdm
 
 item_by_item = pd.read_csv('item_wise.csv')
 os.chdir(
     join('..', '..', '..', 'Dropbox', 'Data', 'FAN_ET', 'Badanie P', '2017-05-06_Badanie_P', 'BadanieP_FAN_ET',
          'Scripts'))
-# %%
+
+# Localisations of region of interest in data.
 ROIS = {
-    'P1': [(-  15, 280), (235, 30)],
-    'P2': [(-  15, 680), (235, 430)],
-    'P3': [(-  15, 1080), (235, 830)],
+    'P1': [(- 15, 280), (235, 30)],
+    'P2': [(- 15, 680), (235, 430)],
+    'P3': [(- 15, 1080), (235, 830)],
     'A': [(+ 465, 520), (715, 270)],
     'B': [(+ 955, 520), (1205, 270)],
     'C': [(+1445, 520), (1695, 270)],
@@ -27,7 +30,32 @@ ROIS = {
 ROIS_ORDER = ['P1', 'P2', 'P3', 'A', 'B', 'C', 'D', 'E', 'F']
 
 
+class CONDITIONS(Enum):
+    LOW_WMC = auto()
+    MED_WMC = auto()
+    HIGH_WMC = auto()
+    FULL = auto()
+    CORR = auto()
+    ERR = auto()
+    TIME_SHORT = auto()
+    TIME_MED = auto()
+    TIME_LONG = auto()
+    LEV_EASY = auto()
+    LEV_MED = auto()
+    LEV_HARD = auto()
+
+
+DEBUG = False
+CONDITION = CONDITIONS.TIME_MED
+
+
 def where_in_list(where, what):
+    """
+    Func that determine position of ROI in raw data yaml list.
+    :param where: List of dicts, from experiment description yaml
+    :param what: Name of ROI to find, eg. A, B, C, P1
+    :return: Pos in list
+    """
     bound = len(where)
     tmp = map(lambda x: x['name'] == what, where)
     return [idx for idx, _ in zip(range(bound), tmp) if _]
@@ -48,11 +76,13 @@ LAB_TO_LEV = {'TRAINING': '[1, 2, 3, 4, 5, 6]',
               'EASY': '[1, 4, 4, 5, 5, 6]',
               'MEDIUM': '[1, 3, 3, 4, 4, 6]',
               'HARD': '[1, 2, 2, 3, 3, 6]'}
+
 LEV_TO_LAB = {v: k for k, v in LAB_TO_LEV.items()}
 
 RESULTS = list()
 TRENING_TRIALS = [1, 2, 3]
-# %%
+
+# LOAD DATA
 SACC_FOLDER = os.path.join('..', 'Dane trackingowe', 'sacc')
 BEH_FOLDER = os.path.join('..', 'results', 'beh')
 FIX_FOLDER = os.path.join('..', 'Dane trackingowe', 'fix')
@@ -68,13 +98,14 @@ yaml_files = os.listdir(YAML_FOLDER)
 
 ID_GF_WMC = pd.read_csv(join('..', 'results', 'ID_GF_WMC.csv'))
 
-low_wmc_band = ID_GF_WMC.WMC.quantile(1/3)
-high_wmc_band = ID_GF_WMC.WMC.quantile(2/3)
+# split participants on order of Working memory capacity category.
+
+low_wmc_band = ID_GF_WMC.WMC.quantile(1 / 3)
+high_wmc_band = ID_GF_WMC.WMC.quantile(2 / 3)
 
 low_wmc = ID_GF_WMC[(ID_GF_WMC.WMC.between(-20, low_wmc_band))].PART_ID.tolist()
 med_wmc = ID_GF_WMC[(ID_GF_WMC.WMC.between(low_wmc_band, high_wmc_band))].PART_ID.tolist()
 high_wmc = ID_GF_WMC[(ID_GF_WMC.WMC.between(high_wmc_band, 20))].PART_ID.tolist()
-import random
 
 Lmin = 0
 Lmax = 120
@@ -85,30 +116,25 @@ res = [list() for _ in range(Lmin, Lmax)]
 FOx = [list() for _ in range(Lmin, Lmax)]
 RMx = [list() for _ in range(Lmin, Lmax)]
 no_fix_in_sec = 0
-# sacc_files = [random.choice(sacc_files)]
+
+if DEBUG:
+    sacc_files = [x for x in sacc_files if '25F' in x]
+    # sacc_files = [random.choice(sacc_files)]
+
 with tqdm(total=len(sacc_files)) as pbar:
     for part_id in sacc_files:  # for each participant
         pbar.set_postfix(file=part_id)
         pbar.update(1)
 
+        # Load Data
         part_id = part_id.split('_')[0]
+
         sacc_data = pd.read_csv(os.path.join(SACC_FOLDER, part_id + '_sacc.csv')).drop('Unnamed: 0', 1)
         sacc_idx = sacc_data.block.unique()
 
         beh_data = pd.read_csv(os.path.join(BEH_FOLDER, part_id + '_beh.csv'))
+        beh_data.set_index(beh_data.index + 1, inplace=True)
         beh_data['corr_and_accept'] = (beh_data['corr'] & beh_data['ans_accept'] & (beh_data['rt'] > 10.0))
-
-        full_index = beh_data[beh_data['rt'] > 10.0].index
-        corr_index = beh_data[beh_data['corr_and_accept']].index
-        err_index = beh_data[(((~ beh_data['corr']) | (~beh_data['ans_accept'])) & (beh_data['rt'] > 10.0))].index
-
-        lev_easy = beh_data[(beh_data.answers.map(LEV_TO_LAB) == 'EASY') & (beh_data['rt'] > 10.0)].index
-        lev_med = beh_data[(beh_data.answers.map(LEV_TO_LAB) == 'MEDIUM') & (beh_data['rt'] > 10.0)].index
-        lev_hard = beh_data[(beh_data.answers.map(LEV_TO_LAB) == 'HARD') & (beh_data['rt'] > 10.0)].index
-
-        time_short = beh_data[beh_data.rt.between(10.0, 40.0)].index
-        time_med = beh_data[beh_data.rt.between(40.0, 80.0)].index
-        time_long = beh_data[beh_data.rt.between(80.0, 121.0)].index
 
         fix_data = pd.read_csv(os.path.join(FIX_FOLDER, part_id + '_fix.csv')).drop('Unnamed: 0', 1)
         fix_idx = fix_data.block.unique()
@@ -124,13 +150,18 @@ with tqdm(total=len(sacc_files)) as pbar:
         problems += yaml_data['list_of_blocks'][1]['experiment_elements'][1:]
         problems += yaml_data['list_of_blocks'][2]['experiment_elements'][1:]
         part_id = part_id.split('F')[0] if 'FEMALE' in part_id else part_id.split('M')[0]
-
-        # if int(part_id) not in low_wmc:
-        #     continue
-
         index_data = pd.read_csv(os.path.join('..', 'results', 'FAN_ET_aggr.csv'))
         index_data = index_data[index_data.Part_id == int(part_id)]
 
+        if CONDITION == CONDITIONS.LOW_WMC:
+            if int(part_id) not in low_wmc:
+                continue
+        if CONDITION == CONDITIONS.MED_WMC:
+            if int(part_id) not in med_wmc:
+                continue
+        if CONDITION == CONDITIONS.HIGH_WMC:
+            if int(part_id) not in high_wmc:
+                continue
         # remove broken trials
         index = set(sacc_idx).intersection(fix_idx).intersection(raw_idx)
         # remove training
@@ -138,33 +169,96 @@ with tqdm(total=len(sacc_files)) as pbar:
         index.discard(2)
         index.discard(3)
         index = sorted(list(index))
-        index = [x for x in index if x <= 45]
-        # %%
 
-        for idx in index:
-            sacc_item = sacc_data[sacc_data.block == idx]
-            fix_item = fix_data[fix_data.block == idx]
+        # Get categories
+        # full_index = beh_data[(beh_data['rt'] > 10.0) & (beh_data['ans_accept'])].index
+        # corr_index = beh_data[beh_data['corr_and_accept']].index
+        # err_index = beh_data[(((~ beh_data['corr']) | (~beh_data['ans_accept'])) & (beh_data['rt'] > 10.0))].index
+        #
+        # lev_easy = beh_data[(beh_data.answers.map(LEV_TO_LAB) == 'EASY') & (beh_data['rt'] > 10.0)].index
+        # lev_med = beh_data[(beh_data.answers.map(LEV_TO_LAB) == 'MEDIUM') & (beh_data['rt'] > 10.0)].index
+        # lev_hard = beh_data[(beh_data.answers.map(LEV_TO_LAB) == 'HARD') & (beh_data['rt'] > 10.0)].index
+        #
+        # time_short = beh_data[beh_data.rt.between(10.0, 40.0)].index
+        # time_med = beh_data[beh_data.rt.between(40.0, 80.0)].index
+        # time_long = beh_data[beh_data.rt.between(80.0, 121.0)].index
+
+        # if int(part_id) not in low_wmc:
+        #     continue
+        for idx in index:  # iterate only over correct trials
+
+            # Due to problems at the level of data acquisition, some indexes ar shifted.
+            beh_idx = idx
+            # Full shift
+            if part_id in ['172', '14', '130', '86', '165', '62', '22', '68', '144']:
+                beh_idx = idx - 1
+
+            if part_id in ['142', '83', '150']:
+                if idx >= 26:
+                    beh_idx = idx - 1
+
+            if part_id in ['50', '144']:
+                if idx >= 26:
+                    beh_idx = idx - 2
+
+            if part_id in ['52']:
+                if idx in range(13, 20):
+                    beh_idx = idx - 1
+                if idx >= 20:
+                    beh_idx = idx - 2
+
             raw_item = raw_data[raw_data.block == idx]
-            beh_item = beh_data.ix[idx - 1]
-            problem = problems[idx - 1]
+            beh_item = beh_data.ix[beh_idx]
 
-            if beh_item.rt >= 120.0:
-                print('rt: {}'.format(beh_item.rt))
-                continue
             start_stamp = int(raw_item.head(1).time.values[0])
             end_stamp = int(raw_item.tail(1).time.values[0])
 
-            a = (len(range(start_stamp, end_stamp, 1000)))
-            # if (a < 80.0) or (a > 120.0):
+            # a = (len(range(start_stamp, end_stamp, 1000)))
+            # if abs(beh_item.rt - a) > 1:
+            #     print('ID: {} IDX: {} BEH_IDX: {} RT: {} real: {}'.format(part_id, idx, beh_idx, beh_item.rt, a))
             #     continue
-            if idx not in err_index:
+
+            sacc_item = sacc_data[sacc_data.block == idx]
+            fix_item = fix_data[fix_data.block == idx]
+            problem = problems[idx - 1]
+
+            # trials to remove
+            if not (10.0 < beh_item.rt < 120.0):
                 continue
-
-
 
             if ((end_stamp - start_stamp) / 1000.0) > 120.0:
                 print('stamp: {}'.format((end_stamp - start_stamp) / 1000.0))
                 continue
+
+            # Select condition
+            if CONDITION == CONDITIONS.FULL:
+                if not beh_item.ans_accept:
+                    continue
+            if CONDITION == CONDITIONS.CORR:
+                if not (beh_item['corr'] and beh_item['ans_accept']):
+                    continue
+            if CONDITION == CONDITIONS.ERR:
+                if (not beh_item['corr']) or (not beh_item['ans_accept']):
+                    continue
+            if CONDITION == CONDITIONS.LEV_EASY:
+                if LEV_TO_LAB[beh_item.answers] != 'EASY':
+                    continue
+            if CONDITION == CONDITIONS.LEV_MED:
+                if LEV_TO_LAB[beh_item.answers] != 'MEDIUM':
+                    continue
+            if CONDITION == CONDITIONS.LEV_HARD:
+                if LEV_TO_LAB[beh_item.answers] != 'HARD':
+                    continue
+            if CONDITION == CONDITIONS.TIME_SHORT:
+                if not (10.0 < beh_item.rt < 40.0):
+                    continue
+            if CONDITION == CONDITIONS.TIME_MED:
+                if not (40.0 < beh_item.rt < 80.0):
+                    continue
+            if CONDITION == CONDITIONS.TIME_LONG:
+                if not (80.0 < beh_item.rt < 120.0):
+                    continue
+
             Kx.append(beh_item.rt)
 
             for idx, start in enumerate(range(start_stamp, end_stamp, 1000)):  # iterate over seconds
@@ -210,7 +304,6 @@ with tqdm(total=len(sacc_files)) as pbar:
                 else:
                     no_fix_in_sec += 1
 
-
 K = list()
 Kx = pd.Series(Kx)
 for l_bound in range(Lmin, Lmax):
@@ -228,8 +321,21 @@ df['AVG_RMx'] = df.RMx / df.RMk
 
 print('No fix in sec:{}'.format(no_fix_in_sec))
 
-
-
 dat = time.localtime()
 filename = '{}_{}_{}_{}:{}'.format(dat.tm_year, dat.tm_mon, dat.tm_mday, dat.tm_hour, dat.tm_min)
-df.to_csv(join('results', 'dynamics_err_' + filename + '.csv'))
+
+cond = {
+    CONDITIONS.FULL: 'full',
+    CONDITIONS.CORR: 'corr',
+    CONDITIONS.ERR: 'err',
+    CONDITIONS.LOW_WMC: 'wmc_low',
+    CONDITIONS.MED_WMC: 'wmc_med',
+    CONDITIONS.HIGH_WMC: 'wmc_high',
+    CONDITIONS.TIME_SHORT: 'time_short',
+    CONDITIONS.TIME_MED: 'time_med',
+    CONDITIONS.TIME_LONG: 'time_long',
+    CONDITIONS.LEV_EASY: 'lev_easy',
+    CONDITIONS.LEV_MED: 'lev_med',
+    CONDITIONS.LEV_HARD: 'lev_hard'
+}
+df.to_csv(join('results', 'dynamics_' + cond[CONDITION] + '_' + filename + '.csv'))
